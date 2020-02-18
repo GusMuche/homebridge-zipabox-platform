3b251210e895a592ea9a9d881fa78d96aa8e5183
GusMuche <gusmuche@gmail.com>
2020-02-18 19:19:29 +0100

    Before changing plugin structure to try better cache manager

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 'use strict'
 
-var ZipaPlatform = require('./lib/platform');
-
-//var Accessory, Service, Characteristic; // TODO delete this if not necessary
-
 module.exports = function(homebridge) {
   /* Platform plugin to be considered as dynamic
   registerPlatform(pluginName, platformName, constructor, dynamic), dynamic must be true */
+  let ZipaPlatform = require('./lib/platform')(homebridge);
   homebridge.registerPlatform("homebridge-zipabox-platform", "Zipabox", ZipaPlatform, true);
 }
