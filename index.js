94364bf1e64047f30b8a0dbb3652b7eebf50040e
GusMuche <gusmuche@gmail.com>
2021-05-24 12:40:20 +0200

    First getValue correction

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ module.exports = function(homebridge) {
   /* Platform plugin to be considered as dynamic
   registerPlatform(pluginName, platformName, constructor, dynamic), dynamic must be true */
   let ZipaPlatform = require('./lib/platform')(homebridge);
-  homebridge.registerPlatform("homebridge-zipabox-platform-dev", "ZipaboxPlatform-dev", ZipaPlatform, true);
+  homebridge.registerPlatform("homebridge-zipabox-platform-dev", "ZipaboxPlatform-dev", ZipaPlatform, true); // also change the addAccessory method if platform name change
 }
