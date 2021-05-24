cc6a23f77d6ad9df58f381c06577dfa0b1b43beb
GusMuche <gusmuche@gmail.com>
2021-05-24 11:23:36 +0200

    Try to use 2package simultaneously

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ module.exports = function(homebridge) {
   /* Platform plugin to be considered as dynamic
   registerPlatform(pluginName, platformName, constructor, dynamic), dynamic must be true */
   let ZipaPlatform = require('./lib/platform')(homebridge);
-  homebridge.registerPlatform("homebridge-zipabox-platform", "ZipaboxPlatform", ZipaPlatform, true);
+  homebridge.registerPlatform("homebridge-zipabox-platform-dev", "ZipaboxPlatform", ZipaPlatform, true);
 }
